f35c919476fa58ae756fe16fd3606ff1ba037452
shemanifisher <shemani@fisherits.com>
2020-04-14 09:34:24 +0100

    fix font

--- src/app/generate/generate.component.ts
+++ src/app/generate/generate.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-generate',
   templateUrl: './generate.component.html',
   styleUrls: ['./generate.component.scss']
 })
 export class GenerateComponent implements OnInit {
-  
+
   title = '';
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-      this.route.queryParamMap.subscribe(queryParams => {
-      this.title = queryParams.get("word")
-     })
+    this.route.queryParamMap.subscribe(queryParams => {
+      // this.title = queryParams.get("word")
+      this.title = 'Coronvirushs';
+      const getFontSize = (textLength) => {
+        console.log(textLength);
+        const baseSize = 35;
+        if (textLength >= baseSize) {
+          textLength = baseSize - 2;
+          const fontSize = baseSize - textLength;
+          return `${fontSize}vmax`;
+        }
+        const fontSize = baseSize - textLength;
+        return `${fontSize}vmin`;
+      }
+
+      // const boxes = this.title.split(' ');
+
+      // boxes.forEach(box => {
+      //   console.log(box)
+      //   $('.fittext').css('font-size', getFontSize(box.length));
+      // });
+
+      $('.fittext').css('font-size', getFontSize(this.title.length));
+
+    });
   }
 }
